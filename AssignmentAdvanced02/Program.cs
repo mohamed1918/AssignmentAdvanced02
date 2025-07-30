813b4cb819d5b0880c0229abc6d23920f86b3839
mohamed1918 <memoohossam2004@gmail.com>
2025-07-30 18:42:40 +0300

    Q4

--- AssignmentAdvanced02/Program.cs
+++ AssignmentAdvanced02/Program.cs
@@ -28,11 +28,35 @@
             #endregion
 
             #region Q3
-            Queue<int> q1 = new Queue<int>(new[] { 1, 2, 3, 4, 5 });
-            Stack<int> s1 = new Stack<int>();
-            while (q1.Count > 0) s1.Push(q1.Dequeue());
-            while (s1.Count > 0) q1.Enqueue(s1.Pop());
-            Console.WriteLine(string.Join(", ", q1));
+            //Queue<int> q1 = new Queue<int>(new[] { 1, 2, 3, 4, 5 });
+            //Stack<int> s1 = new Stack<int>();
+            //while (q1.Count > 0) s1.Push(q1.Dequeue());
+            //while (s1.Count > 0) q1.Enqueue(s1.Pop());
+            //Console.WriteLine(string.Join(", ", q1));
+            #endregion
+
+            #region Q4
+            string input = "[()]{}";
+            //string input = "[()]{";
+            Stack<char> stack1 = new Stack<char>();
+            Dictionary<char, char> pairs = new Dictionary<char, char>
+            {
+                { ')', '(' }, { ']', '[' }, { '}', '{' }
+            };
+            bool balanced = true;
+            foreach (char c in input)
+            {
+                if ("([{".Contains(c)) stack1.Push(c);
+                else if (")]}".Contains(c))
+                {
+                    if (stack1.Count == 0 || stack1.Pop() != pairs[c])
+                    {
+                        balanced = false;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(balanced && stack1.Count == 0 ? "Balanced" : "Not Balanced");
             #endregion
         }
     }
