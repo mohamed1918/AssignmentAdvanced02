dc125e35c0c6f4514385bb9a3a9e5d23ca7062f7
mohamed1918 <memoohossam2004@gmail.com>
2025-07-30 18:36:01 +0300

    Q2

--- AssignmentAdvanced02/Program.cs
+++ AssignmentAdvanced02/Program.cs
@@ -5,11 +5,26 @@
         static void Main(string[] args)
         {
             #region Q1
-            int[] arr1 = { 11, 5, 3 };
-            int[] query = { 1, 5, 13 };
-            foreach (int x in query)
-                Console.WriteLine($"x={x} → {arr1.Count(num => num > x)}");
-            
+            //int[] arr1 = { 11, 5, 3 };
+            //int[] query = { 1, 5, 13 };
+            //foreach (int x in query)
+            //    Console.WriteLine($"x={x} → {arr1.Count(num => num > x)}");
+
+            #endregion
+
+            #region Q2
+            int[] arr1 = { 1, 3, 2, 3, 1 };
+            //int[] arr1 = { 1, 3, 2, 3, 3 };
+            bool isPalindrome = true;
+            for (int i = 0; i < arr1.Length / 2; i++)
+            {
+                if (arr1[i] != arr1[arr1.Length - i - 1])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            Console.WriteLine(isPalindrome ? "yes" : "no");
             #endregion
         }
     }
