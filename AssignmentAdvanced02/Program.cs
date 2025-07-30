abbdffc16c6234146c83758800e95c7ab3a942b5
mohamed1918 <memoohossam2004@gmail.com>
2025-07-30 18:29:27 +0300

    Q1

--- AssignmentAdvanced02/Program.cs
+++ AssignmentAdvanced02/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
+            #region Q1
+            int[] arr1 = { 11, 5, 3 };
+            int[] query = { 1, 5, 13 };
+            foreach (int x in query)
+                Console.WriteLine($"x={x} → {arr1.Count(num => num > x)}");
             
+            #endregion
         }
     }
 }
