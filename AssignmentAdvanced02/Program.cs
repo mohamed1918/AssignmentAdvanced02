0e712ceb509bdf8a94884692d3e184b7054c9cc9
mohamed1918 <memoohossam2004@gmail.com>
2025-07-30 18:38:56 +0300

    Q3

--- AssignmentAdvanced02/Program.cs
+++ AssignmentAdvanced02/Program.cs
@@ -13,18 +13,26 @@
             #endregion
 
             #region Q2
-            int[] arr1 = { 1, 3, 2, 3, 1 };
-            //int[] arr1 = { 1, 3, 2, 3, 3 };
-            bool isPalindrome = true;
-            for (int i = 0; i < arr1.Length / 2; i++)
-            {
-                if (arr1[i] != arr1[arr1.Length - i - 1])
-                {
-                    isPalindrome = false;
-                    break;
-                }
-            }
-            Console.WriteLine(isPalindrome ? "yes" : "no");
+            //int[] arr1 = { 1, 3, 2, 3, 1 };
+            ////int[] arr1 = { 1, 3, 2, 3, 3 };
+            //bool isPalindrome = true;
+            //for (int i = 0; i < arr1.Length / 2; i++)
+            //{
+            //    if (arr1[i] != arr1[arr1.Length - i - 1])
+            //    {
+            //        isPalindrome = false;
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine(isPalindrome ? "yes" : "no");
+            #endregion
+
+            #region Q3
+            Queue<int> q1 = new Queue<int>(new[] { 1, 2, 3, 4, 5 });
+            Stack<int> s1 = new Stack<int>();
+            while (q1.Count > 0) s1.Push(q1.Dequeue());
+            while (s1.Count > 0) q1.Enqueue(s1.Pop());
+            Console.WriteLine(string.Join(", ", q1));
             #endregion
         }
     }
